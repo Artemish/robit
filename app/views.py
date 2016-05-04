9afff1c1cdb52ad5d47e0859a3ac8e3d3d5cc084
yancyribbens <mitch.cro@gmail.com>
2016-05-04 22:36:08 +0000

    add motion commands to view

--- app/views.py
+++ app/views.py
@@ -1,9 +1,34 @@
 from flask import render_template
 from app import app
+import robot
 
 @app.route('/')
 @app.route('/index')
 def index():
+    user = {'nickname': 'Miguel'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
+
+@app.route('/forward')
+def forward():
+    robot.f()
+    user = {'nickname': 'Miguel'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
+
+@app.route('/back')
+def back():
+    robot.b()
+    user = {'nickname': 'Miguel'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
+
+@app.route('/stop')
+def stop():
+    robot.s()
     user = {'nickname': 'Miguel'}  # fake user
     return render_template('index.html',
                            title='Home',
