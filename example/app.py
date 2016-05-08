4f72db5a11b37a735ee242203a4b5c059d33574f
Mitch <mitch.cro@gmail.com>
2016-05-08 04:13:28 +0000

    first working socket

---
+++ example/app.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+# Set this variable to "threading", "eventlet" or "gevent" to test the
+# different async modes, or leave it set to None for the application to choose
+# the best option based on available packages.
+async_mode = None
+
+if async_mode is None:
+    try:
+        import eventlet
+        async_mode = 'eventlet'
+    except ImportError:
+        pass
+
+    if async_mode is None:
+        try:
+            from gevent import monkey
+            async_mode = 'gevent'
+        except ImportError:
+            pass
+
+    if async_mode is None:
+        async_mode = 'threading'
+
+    print('async_mode is ' + async_mode)
+
+# monkey patching is necessary because this application uses a background
+# thread
+if async_mode == 'eventlet':
+    import eventlet
+    eventlet.monkey_patch()
+elif async_mode == 'gevent':
+    from gevent import monkey
+    monkey.patch_all()
+
+import time
+import robot
+import code
+from threading import Thread
+from flask import Flask, render_template, session, request
+from flask_socketio import SocketIO, emit, join_room, leave_room, \
+    close_room, rooms, disconnect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('join', namespace='/test')
+def join(message):
+    x = int(message['x_pos'])
+    y = int(message['y_pos'])
+    robot.joystick(x,y)
+
+def move(message):
+    x = int(message['room'])
+    y = int(message['room'])
+    robot.joystick(x,y)
+
+if __name__ == '__main__':
+    socketio.run(app,host='0.0.0.0', debug=True)
